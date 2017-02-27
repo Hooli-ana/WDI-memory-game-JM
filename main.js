48c2ce4301a94508c92eddc027b867b20a9caade
cyberhooli <julianamichelsen@Julis-MacBook-Air.local>
2017-02-27 00:05:18 -0600

    exercise 11 completed but cards not flipping; need to review where imgs saved)

--- main.js
+++ main.js
@@ -1,24 +1,48 @@
 console.log("JS file is connected to HTML! Woo!")
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-
-if (cardTwo === cardFour) {
-	alert ('Sorry, try again.');
-} else if (cardTwo === cardOne) {
-	alert ('You found a match!');
- } else if (cardThree === cardFour){
-	alert ('You found a match!'); 
-} else if (cardThree === cardTwo){
-	alert ('Sorry, try again.'); 
-
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+ 
 var board = document.getElementByID('game-board');
+function createBoard() {
+	for (var i = 0; i<cards.length; i++) {
+		  var cardElement=document.createElement('div');
+		  cardElement.className= 'card';
+		  cardElement.setAttribute('data-card', cards[i]);
+	}
+	for (var i=0; i < cards.length; i++) {
+		cardElement.addEventListener('click', isTwoCards);
+	}
+}
+
 function createCards() {
-for (var i = 0; i<cards.length; i++) {
-  var cardElement=.createElement('div').className= 'card';
+	for (var i = 0; i<cards.length; i++) {
+  var cardElement=.createElement('div')
+  cardElement.className= 'card';
 }
 
 board.appendChild(cardElement);
+board.appendChild(cardElement);
+
+
+}
+function isTwoCards() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	this.innerHTML = '<img src = "king.png" alt= "King">';
+} else {
+	this.innerHTML = '<img src = "queen.png" alt = "Queen">';
+}
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = []
+	}
+	
+}
+function isMatch(cards) {
+if (cards[0] === cards[1]) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+	}
+}
 
 
