83e28346881d50c53304fee35c545423f0aa93bf
cyberhooli <julimichelsen@gmail.com>
2017-02-27 13:47:30 -0600

    final version
    fixed images and click issue

--- main.js
+++ main.js
@@ -1,48 +1,38 @@
 console.log("JS file is connected to HTML! Woo!")
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
- 
-var board = document.getElementByID('game-board');
+
+var board = document.getElementById('game-board');
 function createBoard() {
-	for (var i = 0; i<cards.length; i++) {
-		  var cardElement=document.createElement('div');
+	for (var i=0; i<cards.length; i++) {
+		  var cardElement = document.createElement('div');
 		  cardElement.className= 'card';
 		  cardElement.setAttribute('data-card', cards[i]);
+			cardElement.addEventListener('click', isTwoCards);
+	board.appendChild(cardElement);
+	board.appendChild(cardElement);
 	}
-	for (var i=0; i < cards.length; i++) {
-		cardElement.addEventListener('click', isTwoCards);
-	}
-}
-
-function createCards() {
-	for (var i = 0; i<cards.length; i++) {
-  var cardElement=.createElement('div')
-  cardElement.className= 'card';
-}
-
-board.appendChild(cardElement);
-board.appendChild(cardElement);
-
-
 }
 function isTwoCards() {
 	cardsInPlay.push(this.getAttribute('data-card'));
-	this.innerHTML = '<img src = "king.png" alt= "King">';
-} else {
-	this.innerHTML = '<img src = "queen.png" alt = "Queen">';
-}
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='https://cdn.pixabay.com/photo/2013/07/13/10/43/cards-157648_960_720.png' alt='King'>";
+		} else {
+		this.innerHTML = "<img src='https://cdn.pixabay.com/photo/2013/07/13/10/43/cards-157644_960_720.png' alt='Queen'>";
+	}
 	if (cardsInPlay.length === 2) {
 		isMatch(cardsInPlay);
-		cardsInPlay = []
+		cardsInPlay = [];
 	}
-	
 }
 function isMatch(cards) {
-if (cards[0] === cards[1]) {
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again.");
+	if (cards[0] === cards[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
 	}
 }
+createBoard();
 
 
